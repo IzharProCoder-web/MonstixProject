ebf67655745c35348c03fa1d7e870cce32963b56
Izhar Khan <itzizharkhan2@gmail.com>
2025-10-08 19:33:54 +0500

    change the the url

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://monstix-project-frontend.vercel.app",
+    origin: "https://monstix-project-frontend.vercel.app/",
     credentials: true,
   })
 );
