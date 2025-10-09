b5c07bfbbc091cf3c7b4066b0ea9f5a3e2effb23
Izhar Khan <itzizharkhan2@gmail.com>
2025-10-09 12:26:22 +0500

    change the api

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ await connectDb()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: " http://localhost:5173",
+  origin: 'https://monstix-project-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
